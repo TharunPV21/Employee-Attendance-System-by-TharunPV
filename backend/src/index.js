956ebffe20858169898c81589fa914d506a8215a
TharunPV21 <167400486+TharunPV21@users.noreply.github.com>
2026-02-17 04:57:09 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const tharunDbConnect = require('./config/tharunDbConnect.js');
+const tharunDbConnect = require('./config/tharunDbConnect');
 
 const authRoutes = require('./routes/authRoutes');
 const attendanceRoutes = require('./routes/attendanceRoutes');
